e188ffce6903c9840fd24c20983b1081ef3139a2
henke <heos2200@student.miun.se>
2024-09-30 16:02:38 +0200

    tested removing jpanel extension...

--- src/main/java/al/tonikolaba/main/BatBatGame.java
+++ src/main/java/al/tonikolaba/main/BatBatGame.java
@@ -10,9 +10,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BatBatGame extends JFrame implements CommandLineRunner {
+public class BatBatGame implements CommandLineRunner {
 
-	private static final long serialVersionUID = -437004379167511593L;
 
 	@Override
 	public void run(String... arg0) throws Exception {
@@ -20,7 +19,7 @@ public class BatBatGame extends JFrame implements CommandLineRunner {
 			@Override
 			public void run() {
 				try {
-					JFrame window = new JFrame("BatBat Game \u2122");
+					JFrame window = new JFrame("BatBat Game ™");
 					window.add(new GamePanel());
 					window.setContentPane(new GamePanel());
 					window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
